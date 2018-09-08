c9b8d6c46816013ae4a92ab48eb49807f012179e
TALAT <medasmac1@TalatMacPro.local>
2018-09-08 12:10:49 +0400

    configuration parameters changed

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@ module.exports = {
     env: process.env.NODE_ENV || "development",
     port: process.env.PORT || 3000,
     from: process.env.USER || "sender@mail.com",// sender address
-    to : process.env.TO || "munquasim4@gmail.com;munquasim3@gmail.com;talat@medasuae.com",// list of receivers
+    to : process.env.TO || "xyz@medasuae.com;abc@medasuae.com",// list of receivers
     service : process.env.SERVICE || 'gmail',
     host : process.env.HOST || "hostaddress",
     emailPort : process.env.EMAILPORT || 587,
-    username : process.env.username || "username", 
-    pass : process.env.DB_PSW || '85----85',
+    username : process.env.USERNAME || "username", 
+    pass : process.env.PSW || '85----85',
 }
